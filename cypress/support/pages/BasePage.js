976b6c156637efc3d662589efbfc605082ed6313
Alex Andrade <alexander.andrade@gmail.com>
2026-03-30 17:53:48 -0500

    ✨ refactor: add printSomething function to log verification messages in BasePage

--- cypress/support/pages/BasePage.js
+++ cypress/support/pages/BasePage.js
@@ -1,6 +1,11 @@
+function printSomething(variable) {
+    console.log('Testing ""');
+}
+
 export default class BasePage {
 
     verifyContactPageLoaded(urlFragment) {
+        printSomething('Verifying contact page loaded');
         cy.url().should('include', urlFragment);
         cy.title().then((_pageTitle) => {
             cy.log('Page title:', _pageTitle);
