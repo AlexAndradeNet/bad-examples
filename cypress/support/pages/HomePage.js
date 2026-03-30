09386ba536e18f6a149331c6ccd6356b08a3949d
Alex Andrade <alexander.andrade@gmail.com>
2026-03-30 17:31:23 -0500

    ✨ refactor: extend HomePage and RetirementPage classes from BasePage for improved structure

--- cypress/support/pages/HomePage.js
+++ cypress/support/pages/HomePage.js
@@ -1,9 +1,11 @@
+import BasePage from './BasePage';
+
 const _elements = {
     industriesOption: () => cy.contains('#menu-main-menu', 'Industries'),
     retirementAndWealthOption: () => cy.contains('.industry-item', 'Retirement and wealth'),
 };
 
-export default class HomePage {
+export default class HomePage extends BasePage {
     navigateToRetirementAndWealth() {
         _elements.industriesOption().realHover();
         _elements.retirementAndWealthOption().click();
--- cypress/support/pages/RetirementPage.js
+++ cypress/support/pages/RetirementPage.js
@@ -1,10 +1,12 @@
+import BasePage from './BasePage';
+
 const _elements = {
     poweringInnovationSection: () => cy.get('h2.section-title'),
     thirdCard: () => cy.contains('.flip-card-inner', 'AI & Machine learning'),
     letsGetStartedButton: () => cy.contains('a', "Let's get started"),
 };
 
-export default class RetirementPage {
+export default class RetirementPage extends BasePage {
     goToCardAndCopyText(expectedText) {
         _elements.poweringInnovationSection().scrollIntoView();
         // Trigger the lazy loading of the cards
